9c47f7c3628de7a13bf5d4e71781a63984d03f99
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-10 23:53:05 +0800

    登录-初始化login组件

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/login', name: 'login', component: () => import('@/views/login') }
 ]
 
 const router = new VueRouter({
