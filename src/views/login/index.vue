6386e804824969f504688dd933bc7da0fe947931
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-11 11:23:51 +0800

    登录-表单校验(自然方式)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,24 +2,30 @@
   <div class="login-container">
     <div class="login-box">
       <!-- 绘制登录form表单 -->
-      <el-form ref="loginFormRef" :model="loginForm">
-        <el-form-item>
-          <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
+      <el-form ref="loginFormRef" :rules="loginFormRules" :model="loginForm">
+        <el-form-item prop="mobile">
+          <el-input
+            v-model="loginForm.mobile"
+            placeholder="请输入手机号码"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="loginForm.code" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"></el-input>
         </el-form-item>
         <el-form-item>
           <!-- 复选框，单个复选框直接设置v-model即可，接收true/false的信息，表示是否选中 -->
           <el-checkbox v-model="loginForm.xieyi"></el-checkbox>
-          <span>我已阅读并同意
-            <a href="#">用户协议</a>和
+          <span
+            >我已阅读并同意 <a href="#">用户协议</a>和
             <a href="#">隐私条款</a>
           </span>
         </el-form-item>
         <el-form-item>
           <!-- 登录按钮 -->
-          <el-button type="primary" style="width:100%;" @click="login()">登录</el-button>
+          <el-button type="primary" style="width:100%;" @click="login()"
+            >登录</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
@@ -31,6 +37,14 @@ export default {
   name: 'Login',
   data () {
     return {
+      // 表单校验规则
+      loginFormRules: {
+        mobile: [
+          { required: true, message: '手机号码必填' },
+          { pattern: /^1[35789]\d{9}$/, message: '手机号码格式不对' }
+        ],
+        code: [{ required: true, message: '验证码必填' }]
+      },
       // 表单数据对象
       loginForm: {
         mobile: '', // 手机号码
@@ -52,7 +66,7 @@ export default {
 <style lang="less" scoped>
 .login-container {
   height: 100%;
-  background-image: url('../../assets/images/bg01.jpg');
+  background-image: url("../../assets/images/bg01.jpg");
   background-size: 100%;
   display: flex;
   justify-content: center;
