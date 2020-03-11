ebac3567264e3bf356bd8088bf9e415b43ee8d50
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-11 18:20:09 +0800

    文章列表-搜索区域-状态表单域

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,7 +6,14 @@
       </div>
       <div class="text item">
         <el-form ref="ArticleFormRef" :model="searcForm" label-width="100px">
-          <el-form-item label="文章状态："></el-form-item>
+          <el-form-item label="文章状态：">
+            <el-radio v-model="searchForm.status" label="">全部</el-radio>
+            <el-radio v-model="searchForm.status" label="0">草稿</el-radio>
+            <el-radio v-model="searchForm.status" label="1">待审核</el-radio>
+            <el-radio v-model="searchForm.status" label="2">审核通过</el-radio>
+            <el-radio v-model="searchForm.status" label="3">审核失败</el-radio>
+            <el-radio v-model="searchForm.status" label="4">已删除</el-radio>
+          </el-form-item>
           <el-form-item label="频道列表："></el-form-item>
           <el-form-item label="时间选择："></el-form-item>
         </el-form>
@@ -20,7 +27,9 @@ export default {
   name: 'Article',
   data () {
     return {
-      searchForm: {}
+      searchForm: {
+        status: '0' // 文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部
+      }
     }
   }
 }
