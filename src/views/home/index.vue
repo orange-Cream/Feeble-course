c766f3bfd90a018514d59c21ec2d59dd941cd4c8
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-11 17:46:53 +0800

    首页搭建-左侧导航-菜单制作

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,43 @@
 <template>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        style="background-color:rgba(221, 235, 32, 0.3);"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>内容管理</span>
+          </template>
+          <el-menu-item index="2-1">发布文章</el-menu-item>
+          <el-menu-item index="2-2">内容列表</el-menu-item>
+          <el-menu-item index="2-3">评论列表</el-menu-item>
+          <el-menu-item index="2-4">素材管理</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-document"></i>
+            <span>粉丝管理</span>
+          </template>
+          <el-menu-item index="3-1">图文数据</el-menu-item>
+          <el-menu-item index="3-2">粉丝概况</el-menu-item>
+          <el-menu-item index="3-3">粉丝画像</el-menu-item>
+          <el-menu-item index="3-4">粉丝列表</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">账户信息</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-container>
       <el-header>
         <div id="lt">
@@ -55,11 +92,11 @@ export default {
 <style lang="less" scoped>
 .el-container {
   height: 100%;
-   .el-aside {
-    background-color: rgba(20, 240, 221, 0.3);
+  .el-aside {
+    background-color: rgba(221, 235, 32, 0.3);
   }
   .el-header {
-     background-color: rgba(221, 235, 32,0.3);
+    background-color: rgba(221, 235, 32, 0.3);
     display: flex;
     justify-content: space-between;
     padding: 0 10px 0 9px;
