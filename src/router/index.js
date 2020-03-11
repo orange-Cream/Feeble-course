3deb0a88f969ecdd320b2ab342c04a73a6a15553
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-11 13:18:34 +0800

    登录-非登录用户限制访问

--- src/router/index.js
+++ src/router/index.js
@@ -12,4 +12,16 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // 获取用户登录状态信息
+  const userinfo = window.sessionStorage.getItem('userinfo')
+  // 登录状态是大字符串, 非登录状态userinfo是null
+  if (!userinfo && to.path !== '/login') {
+    // 强制跳到登录页面
+    return next('/login')
+  }
+  // 放行
+  next()
+})
+
 export default router
