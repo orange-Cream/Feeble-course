6c4cc95740332e1f546d4aab69184b234f43dcc0
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-11 18:17:53 +0800

    文章列表-搜索区域-卡片区和空表单

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>文章列表页</div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>全部图文</span>
+      </div>
+      <div class="text item">
+        <el-form ref="ArticleFormRef" :model="searcForm" label-width="100px">
+          <el-form-item label="文章状态："></el-form-item>
+          <el-form-item label="频道列表："></el-form-item>
+          <el-form-item label="时间选择："></el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Article'
+  name: 'Article',
+  data () {
+    return {
+      searchForm: {}
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
