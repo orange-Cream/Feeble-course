9b35e129e905302ce97730656976c915e8f1ca16
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-12 16:05:29 +0800

    发表文章-绘制表单

--- src/views/articleadd/index.vue
+++ src/views/articleadd/index.vue
@@ -1,11 +1,79 @@
 <template>
-  <div>发表文章展示</div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>发表文章</span>
+      </div>
+      <div class="text item">
+        <el-form ref="addFormRef" :model="addForm" label-width="120px">
+          <el-form-item label="标题：">
+            <el-input v-model="addForm.title"></el-input>
+          </el-form-item>
+          <el-form-item label="内容：">
+            <el-input v-model="addForm.content"></el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+            <el-radio-group v-model="addForm.cover.type">
+              <el-radio :label="1">单图</el-radio>
+              <el-radio :label="3">三图</el-radio>
+              <el-radio :label="0">无图</el-radio>
+              <el-radio :label="-1">自动</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道：">
+            <el-select
+              v-model="addForm.channel_id"
+              placeholder="请选择"
+              clearable
+            >
+              <el-option
+                v-for="item in channelList"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      channelList: [],
+      addForm: {
+        title: '',
+        content: '',
+        cover: {
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: []
+        },
+        channel_id: ''
+      }
+    }
+  },
+  created () {
+    this.getChannelList()
+  },
+  methods: {
+    // 获取频道列表数据
+    getChannelList () {
+      var pro = this.$http.get('/mp/v1_0/channels')
+      pro.then(result => {
+        if (result.data.message === 'OK') {
+          this.channelList = result.data.data.channels
+        }
+      })
+        .catch(err => {
+          return this.$message.error('获得文章频道错误:' + err)
+        })
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
