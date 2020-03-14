11712fb11b181b4f89f9ac6339cdcdb2f80a9304
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-14 13:20:03 +0800

    素材管理-绘制素材列表结构[C

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>素材管理</div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>素材管理</span>
+      </div>
+      <div class="text item">
+        <ul>
+          <li class="image-box" v-for="item in 20" :key="item">
+            <img src="../../assets/logo.png" alt="" />
+          </li>
+          <div class="image-bot">
+            <el-button
+              type="success"
+              icon="el-icon-star-off"
+              circle
+            ></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+          </div>
+        </ul>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -9,4 +30,23 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.image-box{
+    list-style: none;
+    width: 200px;
+    height: 200px;
+    background-color: #fff;
+    margin: 10px;
+    float: left;
+    border: 1px solid #eee;
+    img{
+        width: 200px;
+        height: 140px;
+    }
+    .image-bot {
+    width: 200px;
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+  }
+}
 </style>
